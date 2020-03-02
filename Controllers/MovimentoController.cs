25259ccb41944a48cccb31a8cfd6ef33c693953a
vgobbi8 <vgobbi.8@gmail.com>
2020-03-01 21:22:23 -0300

    Primeiro Commit

---
+++ Controllers/MovimentoController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using API_FinancialManager_Core.Models;
+
+namespace API_FinancialManager_Core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    [Consumes("application/json")]
+    public class MovimentoController : ControllerBase
+    {
+        [HttpGet]
+        [ActionName("Receber")]
+        [AcceptVerbs("Get")]
+        public MovimentoModel Get()
+        {
+            MovimentoModel m = new MovimentoModel();
+            m.Codigo = 1;
+            m.Descricao = "AAA";
+            m.Valor = 10;
+
+            return m;
+        }
+
+        [HttpPost]
+     //   [ActionName("EnvioRetorno")]
+        public MovimentoModel Post([FromBody] MovimentoModel value)
+        {
+            MovimentoModel movimento = new MovimentoModel();
+            value.teste();
+            return value;
+        }
+
+        // PUT: api/Movimento/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Models/MovimentoModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_FinancialManager_Core.Models
+{
+    public class MovimentoModel
+    {
+        public int Codigo { get; set; }
+        public string Descricao { get; set; }
+        public double Valor { get; set; }
+
+
+        public void teste()
+        {
+            int a = this.Codigo;
+            string b = this.Descricao;
+            double d = this.Valor;
+            //Enums.TiposMovimento c = this.TipoMovimento;
+            //DateTime e = this.DataMovimento;
+            this.Codigo = 100;
+            this.Descricao = "PASSOU PELA API";
+            this.Valor = 1000;
+        }
+    }
+}
